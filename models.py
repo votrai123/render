f5035a8f952d82130f30006814cc3247199f6f43
NGUYEN VO TRAI <58043896+votrai123@users.noreply.github.com>
2024-11-18 19:22:29 +0700

    update models

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import ForeignKey, Column, String, Integer, \
+                    DateTime, create_engine
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
@@ -19,25 +22,84 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+    migrate = Migrate(app, db)
 
 
 '''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
+Book
 
-  id = Column(db.Integer, primary_key=True)
+'''
+class Book(db.Model):
+  __tablename__ = 'books'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(DateTime)
+  authors = relationship('Author', backref='author', lazy = True)
+
+  def __init__(self, title, release_date):
+    self.title = title
+    self.release_date = release_date
+
+  
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date,
+      'authors': list(map(lambda author: author.format(), self.authors))
+    }
+
+
+'''
+Author
+'''
+
+class Author(db.Model):
+  __tablename__ = 'authors'
+
+  id = Column(Integer, primary_key=True)
   name = Column(String)
-  catchphrase = Column(String)
+  age = Column(Integer)
+  gender = Column(String)
+  book_id = Column(Integer, ForeignKey('books.id'), nullable= True)
 
-  def __init__(self, name, catchphrase=""):
+  def __init__(self, name, age, gender, movie_id):
     self.name = name
-    self.catchphrase = catchphrase
+    self.age = age
+    self.gender = gender
+    self.movie_id = movie_id
+
+  
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'catchphrase': self.catchphrase}
+      'age': self.age,
+      'gender': self.gender,
+      'movie_id': self.movie_id,
+    }
+
+
