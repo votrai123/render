0f1ed625d7c5b1d2016acf1adfcba68da4df85aa
NGUYEN VO TRAI <58043896+votrai123@users.noreply.github.com>
2024-11-23 15:27:17 +0700

    change cors

--- app.py
+++ app.py
@@ -12,203 +12,240 @@ def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     @app.after_request
     def after_request(response):
         response.headers.add(
-            'Access-Control-Allow-Headers',
-            'Content-Type,Authorization,true')
+            "Access-Control-Allow-Headers", 
+            "Content-Type,Authorization,true"
+        )
         response.headers.add(
-            'Access-Control-Allow-Methods',
-            'GET,PUT,POST,DELETE,OPTIONS')
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Origin", "*"
+        )
         return response
 
     @app.route('/books', methods=['GET'])
     @requires_auth('view:books')
     def get_books(payload):
-        books = Book.query.all()
-        print(str(books)+ " books ====")
-        books = list(map(lambda book: book.format(), books))
-        return jsonify({
-            "success": True,
-            "books": books
-        })
+        try:
+            books = Book.query.all()
+            print(str(books)+ " books ====")
+            books = list(map(lambda book: book.format(), books))
+            return jsonify({
+                "success": True,
+                "books": books
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
 
     @app.route('/authors', methods=['GET'])
     @requires_auth('view:authors')
     def get_authors(payload):
-        authors = Actor.query.all()
-        print(str(authors)+ " authors ====")
-        authors = list(map(lambda author: author.format(), authors))
-        return jsonify({
-            "success": True,
-            "authors": authors
-        })
+        try:
+            authors = Actor.query.all()
+            print(str(authors)+ " authors ====")
+            authors = list(map(lambda author: author.format(), authors))
+            return jsonify({
+                "success": True,
+                "authors": authors
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
     
     @app.route('/books', methods=['POST'])
     @requires_auth('post:books')
     def create_book(payload):
         body = request.get_json()
+        try:
+            print(str(body)+ " body ====")
+            if body is None:
+                abort(400)
 
-        print(str(body)+ " body ====")
-        if body is None:
+            title = body.get('title', None)
+            content = body.get('content', None)
+            type = body.get('type', None)
+            release_date = body.get('release_date', None)
+            author_id = body.get('author_id', None)
+            print(str(title)+ " title ====")
+            print(str(content)+ " content ====")
+            print(str(type)+ " type ====")
+            print(str(release_date)+ " release_date ====")
+            print(str(author_id)+ " author_id ====")
+
+            if title is None or content is None or release_date is None or type is None or author_id is None:
+                abort(400, "Body Error")
+
+            book = Book(title=title, content = content,release_date=release_date, type = type, author_id=author_id )
+
+            book.insert()
+
+            return jsonify({
+                "success": True
+            })
+        except Exception as e:
+            print(e)
             abort(400)
-
-        title = body.get('title', None)
-        content = body.get('content', None)
-        type = body.get('type', None)
-        release_date = body.get('release_date', None)
-        author_id = body.get('author_id', None)
-        print(str(title)+ " title ====")
-        print(str(content)+ " content ====")
-        print(str(type)+ " type ====")
-        print(str(release_date)+ " release_date ====")
-        print(str(author_id)+ " author_id ====")
-
-        if title is None or content is None or release_date is None or type is None or author_id is None:
-            abort(400, "Body Error")
-
-        book = Book(title=title, content = content,release_date=release_date, type = type, author_id=author_id )
-
-        book.insert()
-
-        return jsonify({
-            "success": True
-        })
 
 
     @app.route('/authors', methods=['POST'])
     @requires_auth('post:authors')
     def create_author(payload):
         body = request.get_json()
-
-        print(str(body)+ " body ====")
-
-        if body is None:
-            abort(400)
-
-        name = body.get('name', None)
-        age = body.get('age', None)
-        gender = body.get('gender', None)
+        new_question = body.get("name")
+        new_answer = body.get("age")
+        new_difficulty = body.get("gender")
         print(str(name)+ " name ====")
-        print(str(age)+ " name ====")
-        print(str(gender)+ " name ====")
+        print(str(age)+ " age ====")
+        print(str(gender)+ " gender ====")
+        try:
+            print(str(body)+ " body ====")
 
-        if name is None or age is None or gender is None:
-            abort(400, "Body Error")
+            if body is None:
+                abort(400)
 
-        author = Author(name=name, age=age, gender=gender)
+            name = body.get('name', None)
+            age = body.get('age', None)
+            gender = body.get('gender', None)
+            print(str(name)+ " name ====")
+            print(str(age)+ " age ====")
+            print(str(gender)+ " gender ====")
 
-        author.insert()
+            if name is None or age is None or gender is None:
+                abort(400, "Body Error")
 
-        return jsonify({
-            "success": True
-        })
+            author = Author(name=name, age=age, gender=gender)
 
+            author.insert()
+
+            return jsonify({
+                "success": True
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
     @app.route('/books/<int:book_id>', methods=['DELETE'])
     @requires_auth('delete:books')
     def delete_book(payload, book_id):
-        book = Book.query.filter(Book.id == book_id).one_or_none()
+        try:
+            book = Book.query.filter(Book.id == book_id).one_or_none()
 
-        if book is None:
-            abort(404, "Not found book " + str(book_id))
+            if book is None:
+                abort(404, "Not found book " + str(book_id))
 
-        book.delete()
+            book.delete()
 
-        return jsonify({
-            'success': True,
-            'book_id': book_id
-        })
+            return jsonify({
+                'success': True,
+                'book_id': book_id
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
 
     @app.route('/authors/<int:author_id>', methods=['DELETE'])
     @requires_auth('delete:authors')
     def delete_author(payload, author_id):
-        author = Author.query.filter(Author.id == author_id).one_or_none()
+        try:
+            author = Author.query.filter(Author.id == author_id).one_or_none()
 
-        if author is None:
-            abort(404, "Not found author" + str(author_id))
+            if author is None:
+                abort(404, "Not found author" + str(author_id))
 
-        author.delete()
+            author.delete()
 
-        return jsonify({
-            'success': True,
-            'author_id': author_id
-        })
+            return jsonify({
+                'success': True,
+                'author_id': author_id
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
 
     @app.route('/books/<int:book_id>', methods=['PATCH'])
     @requires_auth('update:books')
     def update_book(payload, book_id):
+        try:
+            book_update = Book.query.get(book_id)
 
-        book_update = Book.query.get(book_id)
+            if not book_update:
+                abort(404,'Book id' + str(book_id) + ' can not found!')
 
-        if not book_update:
-            abort(404,'Book id' + str(book_id) + ' can not found!')
+            body = request.get_json()
 
-        body = request.get_json()
+            title = body.get('title', None)
+            content = body.get('content', None)
+            type = body.get('type', None)
+            release_date = body.get('release_date', None)
+            author_id = body.get('author_id', None)
 
-        title = body.get('title', None)
-        content = body.get('content', None)
-        type = body.get('type', None)
-        release_date = body.get('release_date', None)
-        author_id = body.get('author_id', None)
+            if title:
+                updated_book.title = title
+            if content:
+                updated_book.content = content
+            if type:
+                updated_book.type = type
+            if release_date:
+                updated_book.release_date = release_date
+            if author_id:
+                author_update.author_id = author_id
 
-        if title:
-            updated_book.title = title
-        if content:
-            updated_book.content = content
-        if type:
-            updated_book.type = type
-        if release_date:
-            updated_book.release_date = release_date
-        if author_id:
-            author_update.author_id = author_id
+            book_update.update()
 
-        book_update.update()
-
-        return jsonify({
-            "success": True,
-            "book": book_update.format()
-        })
+            return jsonify({
+                "success": True,
+                "book": book_update.format()
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
 
     @app.route('/authors/<int:author_id>', methods=['PATCH'])
     @requires_auth('update:authors')
     def update_author(payload, author_id):
-
-        author_update = Author.query.get(author_id)
-
-        if not author_update:
-            abort(404,'Author id: ' + str(author_id) +' can not found!')
-
-        body = request.get_json()
-
-        name = body.get('name', None)
-        age = body.get('age', None)
-        gender = body.get('gender', None)
-
-        if name:
-            author_update.name = name
-        if age:
-            author_update.age = age
-        if gender:
-            author_update.gender = gender
-        
-
         try:
-            author_update.update()
-        except BaseException:
-            abort( 400, "format error" + str(author_id))
+            author_update = Author.query.get(author_id)
 
-        return jsonify({
-            "success": True,
-            "author": author_update.format()
-        })
+            if not author_update:
+                abort(404,'Author id: ' + str(author_id) +' can not found!')
+
+            body = request.get_json()
+
+            name = body.get('name', None)
+            age = body.get('age', None)
+            gender = body.get('gender', None)
+
+            if name:
+                author_update.name = name
+            if age:
+                author_update.age = age
+            if gender:
+                author_update.gender = gender
+
+
+            try:
+                author_update.update()
+            except BaseException:
+                abort( 400, "format error" + str(author_id))
+
+            return jsonify({
+                "success": True,
+                "author": author_update.format()
+            })
+        except Exception as e:
+            print(e)
+            abort(400)
 
     # Error Handling
     '''
@@ -283,4 +320,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
